302a20a338460460299843ee62d0569f62674290
ohyoung <ohyang94e@163.com>
2019-03-24 12:37:53 +0800

    update webpack support checkSyntacticErrors

--- client/config/webpack.common.js
+++ client/config/webpack.common.js
@@ -1,16 +1,20 @@
 const path = require('path')
+const process = require('process')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 const themeConfig = require('../theme.config')
+
+const isDev = process.env.npm_lifecycle_event !== 'prod'
 
 module.exports = {
   entry: {
     app: ['./src/index.tsx']
   },
   output: {
-    path: path.join(__dirname, '../dist'),
+    path: path.resolve(__dirname, '../dist'),
     filename: '[name].bundle_[hash].js',
     chunkFilename: '[id].chunk_[chunkhash].js',
     publicPath: '/'
@@ -18,7 +22,9 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
     plugins: [
-      new TsconfigPathsPlugin({ configFile: path.join(__dirname, '../', 'tsconfig.json') })
+      new TsconfigPathsPlugin({
+        configFile: path.join(__dirname, '../', 'tsconfig.json')
+      })
     ]
   },
   module: {
@@ -28,19 +34,29 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           'babel-loader',
-          'ts-loader'
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: isDev
+            }
+          }
         ]
       },
       {
         test: /\.less$/,
         // exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', {
-          loader: 'less-loader',
-          options: {
-            modifyVars: themeConfig,
-            javascriptEnabled: true
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              modifyVars: themeConfig,
+              javascriptEnabled: true
+            }
           }
-        }]
+        ]
       },
       {
         test: /\.css$/,
@@ -64,6 +80,6 @@ module.exports = {
       template: 'index.html',
       inject: true
     }),
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin()
   ]
 }
--- client/config/webpack.dev.js
+++ client/config/webpack.dev.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const common = require('./webpack.common')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'development',
@@ -17,7 +18,7 @@ module.exports = merge(common, {
       '/api/*': {
         target: 'http://localhost:7001',
         secure: false,
-        bypass: function (req, res, proxyOptions) {
+        bypass: function(req, res, proxyOptions) {
           if (req.headers.accept && req.headers.accept.indexOf('html') !== -1) {
             console.log('Skipping proxy for browser request.')
             return '/index.html'
@@ -30,6 +31,9 @@ module.exports = merge(common, {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin()
+    new webpack.NamedModulesPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      checkSyntacticErrors: true
+    })
   ]
 })
