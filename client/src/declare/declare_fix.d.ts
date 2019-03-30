1eec4dc32bd951a122f77ee592fae6d2e7a7892d
三十文 <1047210087@qq.com>
2019-03-30 21:03:46 +0800

    add axios util and add mock

--- client/src/declare/declare_fix.d.ts
+++ client/src/declare/declare_fix.d.ts
@@ -1,2 +1,3 @@
 // 修复 antd 的 Button 组件的类型校验错误，暂不开放
 // declare module 'antd'
+// declare module 'axios'
---
+++ client/src/utils/requestUtil.ts
@@ -0,0 +1,78 @@
+import axios from 'axios'
+import { message as antdMessage } from 'antd'
+
+const axiosInstance = axios.create({
+  baseURL: '/',
+  timeout: 2000
+})
+
+/**
+ * get 请求
+ * @param url 请求资源地址
+ * @param params 请求参数
+ */
+export function pget(url, params = {}) {
+  try {
+    return axiosInstance.get(url, {
+      params,
+      validateStatus: (status) => {
+        return status >= 200 && status < 300 || status === 700
+      }
+    }).then(response => {
+      const code = response.data.code
+      const message = response.data.msg
+      if (code > 220 || code < 200) {
+        antdMessage.warn(message)
+      }
+      return response.data
+    }).catch(error => {
+      throw error
+    })
+  } catch (err) {
+    console.error(err.type)
+    console.error(err.message)
+  }
+}
+
+/**
+ * post 请求
+ * @param url 请求资源地址
+ * @param params 请求参数
+ */
+export function ppost(url, params = {}) {
+  try {
+    return axios.post(
+      url,
+      params
+    ).then(response => {
+      const code = response.data.code
+      const message = response.data.msg
+      if (code > 220 || code < 200) {
+        antdMessage.warn(message)
+      }
+      return response.data
+    }).catch(error => {
+      throw error
+    })
+  } catch (err) {
+    console.error(err.type)
+    console.error(err.message)
+  }
+}
+
+/**
+ * 多个请求并发
+ * @param requests
+ */
+export function asyncAll(requests = []) {
+  try {
+    return axios.all(requests).then(resultArr => {
+      return resultArr
+    }).catch(error => {
+      throw error
+    })
+  } catch (err) {
+    console.error(err.type)
+    console.error(err.message)
+  }
+}
