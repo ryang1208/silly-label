a7d599cbd058c3f7909ebff13903e2dd576f63be
yangrichard <3327959823@qq.com>
2019-04-09 22:21:26 +0800

    modify validate

--- server/src/main/java/com/label/configuration/CurrentUserMethodArgumentResolver.java
+++ server/src/main/java/com/label/configuration/CurrentUserMethodArgumentResolver.java
@@ -1,8 +1,7 @@
 package com.label.configuration;
 
-import com.label.annoation.CurrentUser;
+import com.label.annotation.CurrentUser;
 import com.label.po.user.LoginUser;
-import com.label.po.user.UserInfo;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -11,22 +10,26 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
-/**
- *  增加方法注入，将含有 @CurrentUser 注解的方法参数注入当前登录用户
- */
+/** 增加方法注入，将含有 @CurrentUser 注解的方法参数注入当前登录用户 */
 public class CurrentUserMethodArgumentResolver implements HandlerMethodArgumentResolver {
-    @Override
-    public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.getParameterType().isAssignableFrom(LoginUser.class)
-                && parameter.hasParameterAnnotation(CurrentUser.class);
-    }
+  @Override
+  public boolean supportsParameter(MethodParameter parameter) {
+    return parameter.getParameterType().isAssignableFrom(LoginUser.class)
+        && parameter.hasParameterAnnotation(CurrentUser.class);
+  }
 
-    @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        LoginUser user = (LoginUser) webRequest.getAttribute("currentUser", RequestAttributes.SCOPE_REQUEST);
-        if (user != null) {
-            return user;
-        }
-        throw new MissingServletRequestPartException("currentUser");
+  @Override
+  public Object resolveArgument(
+      MethodParameter parameter,
+      ModelAndViewContainer mavContainer,
+      NativeWebRequest webRequest,
+      WebDataBinderFactory binderFactory)
+      throws Exception {
+    LoginUser user =
+        (LoginUser) webRequest.getAttribute("currentUser", RequestAttributes.SCOPE_REQUEST);
+    if (user != null) {
+      return user;
     }
+    throw new MissingServletRequestPartException("currentUser");
+  }
 }
--- server/src/main/java/com/label/controller/LoginController.java
+++ server/src/main/java/com/label/controller/LoginController.java
@@ -1,6 +1,6 @@
 package com.label.controller;
 
-import com.label.annoation.CurrentUser;
+import com.label.annotation.CurrentUser;
 import com.label.exception.BusinessException;
 import com.label.po.user.LoginUser;
 import com.label.po.user.UserInfo;
@@ -10,64 +10,58 @@ import com.label.vo.ResultVO;
 import com.label.vo.UserInfoVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/user")
-@Api(value="用户相关接口")
+@Api(value = "用户相关接口")
 public class LoginController {
 
-    @Autowired
-    private UserService userService;
+  @Autowired private UserService userService;
 
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-    @ApiOperation(value = "登录接口",response = String.class)
-    public ResultVO login(HttpServletResponse httpServletResponse, @RequestBody UserInfo userInfo){
-            try {
-                userService.login(httpServletResponse, userInfo);
-                return WebUtils.success();
-            }
-            catch (BusinessException e){
-                return WebUtils.error(201,e.getMessage());
-            }
+  @RequestMapping(value = "/login", method = RequestMethod.POST)
+  @ApiOperation(value = "登录接口", response = String.class)
+  public ResultVO login(HttpServletResponse httpServletResponse, @RequestBody UserInfo userInfo) {
+    try {
+      userService.login(httpServletResponse, userInfo);
+      return WebUtils.success();
+    } catch (BusinessException e) {
+      return WebUtils.error(201, e.getMessage());
     }
+  }
 
-    @RequestMapping(value = "/register",method = RequestMethod.POST)
-    @ApiOperation(value = "注册接口",response = String.class)
-    public ResultVO register(@RequestBody UserInfo userInfo){
-        try {
-            userService.register(userInfo);
-            return WebUtils.success();
-        }catch (BusinessException e){
-            return WebUtils.error(201,e.getMessage());
-        }
+  @RequestMapping(value = "/register", method = RequestMethod.POST)
+  @ApiOperation(value = "注册接口", response = String.class)
+  public ResultVO register(@RequestBody UserInfo userInfo) {
+    try {
+      userService.register(userInfo);
+      return WebUtils.success();
+    } catch (BusinessException e) {
+      return WebUtils.error(201, e.getMessage());
     }
+  }
 
-
-    @RequestMapping(value = "/reset",method = RequestMethod.POST)
-    @ApiOperation(value = "/重置密码",response = String.class)
-    public ResultVO reset(@RequestBody UserInfoVO userInfoVO){
-        try {
-            userService.resetPassword(userInfoVO);
-            return WebUtils.success();
-        }catch (BusinessException e){
-            return WebUtils.error(201,e.getMessage());
-        }
+  @RequestMapping(value = "/reset", method = RequestMethod.POST)
+  @ApiOperation(value = "/重置密码", response = String.class)
+  public ResultVO reset(@RequestBody UserInfoVO userInfoVO) {
+    try {
+      userService.resetPassword(userInfoVO);
+      return WebUtils.success();
+    } catch (BusinessException e) {
+      return WebUtils.error(201, e.getMessage());
     }
+  }
 
-    @RequestMapping(value = "/logout",method = RequestMethod.POST)
-    @ApiOperation(value = "登出功能",response = String.class)
-    public ResultVO logout(HttpServletResponse response,@CurrentUser LoginUser loginUser){
+  @RequestMapping(value = "/logout", method = RequestMethod.POST)
+  @ApiOperation(value = "登出功能", response = String.class)
+  public ResultVO logout(HttpServletResponse response, @CurrentUser LoginUser loginUser) {
 
-        userService.logout(response,loginUser);
-        return WebUtils.success();
-    }
-
-
-
+    userService.logout(response, loginUser);
+    return WebUtils.success();
+  }
 }
